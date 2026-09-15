799d499216cc407c3a44de93b8c77401e71b4515
temitope-israel <omoniyitemitopeisrael@gmail.com>
2026-09-15 10:29:20 +0100

    Fix import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from "@/app/components/Navbar";
-import Hero from "@/app/components/Hero";
-import About from "@/app/components/About";
-import Projects from "./components/Projects";
-import Process from "./components/Process";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Process from "@/components/Process";
+import Footer from "@/components/Footer";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
